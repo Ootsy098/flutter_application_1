77bfd8f6e18e1f5a6f35aab6b1b28c0df0a553bb
Ootsy098 <162300067+Ootsy098@users.noreply.github.com>
2025-11-15 20:15:11 +0100

    improved movement and gravity

--- lib/player.dart
+++ lib/player.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
@@ -6,7 +9,11 @@ import 'package:flutter/widgets.dart';
 const String playerPNG = 'jumper_sprite.png';
 
 class Player extends SpriteComponent with KeyboardHandler {
-  num playerSpeed = 10;
+  late double playerSpeed = 600;
+  late double horzLerpAcc = 15;
+  late double normalJumpV = -1000;
+  late double gravityC = 20;
+  Vector2 velocity = Vector2.zero();
   late Set<LogicalKeyboardKey> keysDown = {};
   final Vector2 screenSize;
 
@@ -25,27 +32,53 @@ class Player extends SpriteComponent with KeyboardHandler {
     } else if (event is KeyUpEvent) {
       keysDown.remove(event.logicalKey);
     }
-
     return true;
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    if (keysDown.contains(LogicalKeyboardKey.arrowLeft)) {
-      move(-1);
-    } else if (keysDown.contains(LogicalKeyboardKey.arrowRight)) {
-      move(1);
+    if (keysDown.contains(LogicalKeyboardKey.arrowLeft) &&
+        !keysDown.contains(LogicalKeyboardKey.arrowRight)) {
+      inputMove(-1, dt);
+    } else if (keysDown.contains(LogicalKeyboardKey.arrowRight) &&
+        !keysDown.contains(LogicalKeyboardKey.arrowLeft)) {
+      inputMove(1, dt);
+    } else {
+      velocity.x = 0;
     }
+    position += velocity * dt;
+    applyGravity();
   }
 
-  void move(int dir) {
-    position.x += dir * playerSpeed;
+  void inputMove(int dir, double dt) {
+    velocity.x = lerpDouble(velocity.x, playerSpeed * dir, horzLerpAcc * dt)!;
+    if (dir == 1 && isFlippedHorizontally) {
+      flipHorizontallyAroundCenter();
+    } else if (dir == -1 && !isFlippedHorizontally) {
+      flipHorizontallyAroundCenter();
+    }
+    teleport();
+  }
 
+  void teleport() {
     if (position.x < -size.x / 2) {
       position.x = screenSize.x + size.x / 2;
     } else if (position.x > screenSize.x + size.x / 2) {
       position.x = -size.x / 2;
     }
+  }
+
+  void applyGravity() {
+    if (position.y < screenSize.y - size.y / 2) {
+      velocity.y += gravityC;
+    } else {
+      jump(normalJumpV);
+      position.y = screenSize.y - size.y / 2;
+    }
+  }
+
+  void jump(double upwardsVelocity) {
+    velocity.y = upwardsVelocity;
   }
 }
