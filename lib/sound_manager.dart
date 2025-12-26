c5b30198f150a538ca955a658aec78bc78397141
Ootsy098 <162300067+Ootsy098@users.noreply.github.com>
2025-12-26 13:09:42 +0100

    feat: add additional sound playback methods for fall, button, propeller, jetpack, and break actions

--- lib/sound_manager.dart
+++ lib/sound_manager.dart
@@ -8,4 +8,24 @@ class SoundManager {
   void playSpringSound() {
     FlameAudio.play('spring.mp3');
   }
+
+  void playFallSound() {
+    FlameAudio.play('fall.mp3');
+  }
+
+  void playButtonSound() {
+    FlameAudio.play('button.wav');
+  }
+
+  void playPropellerSound() {
+    FlameAudio.play('propeller.mp3');
+  }
+
+  void playJetpackSound() {
+    FlameAudio.play('jetpack.mp3');
+  }
+
+  void playBreakSound() {
+    FlameAudio.play('break.wav');
+  }
 }
